bcec145d81c0a557ae941e96e50b7193d64e807e
Qais-ir <qais@orinted-s.com>
2025-08-19 22:40:54 +0300

    employee component

---
+++ Frontend/HR/src/app/components/employees/employees.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  imports: [],
+  templateUrl: './employees.html',
+  styleUrl: './employees.css'
+})
+export class Employees {
+
+  name = "Hello Angular";
+}
